eefbd4b8f81f56db08e5e10d0a5c3ddf27f6125e
hustcc <i@atool.org>
2016-08-18 11:14:14 +0800

    add demo

---
+++ demo/demo.js
@@ -0,0 +1,26 @@
+function do_get_psot_demo() {
+  var xhr = XHR();
+  xhr.get('package.json', {'a': 'b'}, function(r) {
+    r = r.json();
+    alert(r.name + '\n' + r.summary);
+  });
+}
+
+function do_on_function() {
+  var xhr = XHR();
+  xhr.on('success', function(r) {
+    r = r.json();
+    alert(r.name + ' code is here: ' + r.repository.url);
+  });
+  xhr.request('GET', 'package.json', null);
+}
+
+function do_sync_request() {
+  var xhr = XHR(false);
+  xhr.request('DELETE', 'package.json', null, function(r) {
+    r = r.json();
+    alert(r.name + ' code is here: ' + r.repository.url);
+  }, function(r) {
+    alert('Request error: \n' + r.status_code + '\n' + r.status_text);
+  });
+}
